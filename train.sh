72c64253111a28b5ae46eff61d399702f14ee6af
Dxy <denight@qq.com>
2018-05-06 06:17:59 +0800

    add train.py and a shell script to run train.py

---
+++ train.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python ./train.py --logtostderr \
+    --pipeline_config_path=/home/daixinye/workspace/tf_febric_detector/ssd_mobilenet_v1_raccoon.config \
+    --train_dir=/home/daixinye/workspace/tf_febric_detector/train
