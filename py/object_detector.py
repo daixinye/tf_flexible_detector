6acaa9b640cc100eb2c8a1b04fd27f33ebefbdaa
Dxy <denight@qq.com>
2018-05-30 22:23:48 +0800

    update object_detector

--- py/object_detector.py
+++ py/object_detector.py
@@ -22,43 +22,15 @@ from io import StringIO
 from matplotlib import pyplot as plt
 from PIL import Image
 
-# This is needed since the notebook is stored in the object_detection folder.
 from object_detection.utils import ops as utils_ops
-
-if tf.__version__ < '1.4.0':
-  raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
-
-
-# ## Env setup
-
-# In[2]:
-
-
-# This is needed to display the images.
-# get_ipython().magic(u'matplotlib inline')
 
 
 # ## Object detection imports
 # Here are the imports from the object detection module.
 
-# In[3]:
-
-
 from object_detection.utils import label_map_util
 
 from object_detection.utils import visualization_utils as vis_util
-
-
-# # Model preparation 
-
-# ## Variables
-# 
-# Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_CKPT` to point to a new .pb file.  
-# 
-# By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
-
-# In[4]:
-
 
 # What model to download.
 # MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
@@ -67,33 +39,15 @@ from object_detection.utils import visualization_utils as vis_util
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 # PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
-PATH_TO_CKPT = '/home/daixinye/workspace/tf_febric_detector/output/frozen_inference_graph.pb'
+PATH_TO_CKPT = '/home/daixinye/workspace/tf_febric_detector/_train/_train_2/output/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
 # PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
 PATH_TO_LABELS = '/home/daixinye/workspace/tf_febric_detector/label_map.pbtxt'
 
-NUM_CLASSES = 90
-
-
-# ## Download Model
-
-# In[5]:
-
-
-# opener = urllib.request.URLopener()
-# opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-# tar_file = tarfile.open(MODEL_FILE)
-# for file in tar_file.getmembers():
-#   file_name = os.path.basename(file.name)
-#   if 'frozen_inference_graph.pb' in file_name:
-#     tar_file.extract(file, os.getcwd())
-
+NUM_CLASSES = 1
 
 # ## Load a (frozen) Tensorflow model into memory.
-
-# In[6]:
-
 
 detection_graph = tf.Graph()
 with detection_graph.as_default():
@@ -106,9 +60,6 @@ with detection_graph.as_default():
 
 # ## Loading label map
 # Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
-
-# In[7]:
-
 
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
@@ -127,24 +78,17 @@ def load_image_into_numpy_array(image):
 
 
 # # Detection
-
-# In[48]:
-
-
-# For the sake of simplicity we will use only 2 images:
-# image1.jpg
-# image2.jpg
+import tkFileDialog
+filename = tkFileDialog.askopenfilename(initialdir='/home/daixinye/workspace', filetypes=[("*.jpg","*.jpg")])
+if len(filename) == 0:
+  exit()
 # If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
 PATH_TO_TEST_IMAGES_DIR = 'test_images'
 # TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 4) ]
-TEST_IMAGE_PATHS = ['/home/daixinye/workspace/tf_febric_detector/test/8cb21527f9d745d9dc4d538d617bdb92.jpg']
-
+# TEST_IMAGE_PATHS = ['/home/daixinye/workspace/tf_febric_detector/_test/8cb21527f9d745d9dc4d538d617bdb92.jpg']
+TEST_IMAGE_PATHS = [filename]
 # Size, in inches, of the output images.
-IMAGE_SIZE = (24, 16)
-
-
-# In[49]:
-
+IMAGE_SIZE = (15, 10)
 
 def run_inference_for_single_image(image, graph):
   with graph.as_default():
@@ -192,10 +136,6 @@ def run_inference_for_single_image(image, graph):
         output_dict['detection_masks'] = output_dict['detection_masks'][0]
   return output_dict
 
-
-# In[50]:
-
-
 for image_path in TEST_IMAGE_PATHS:
   image = Image.open(image_path)
   # the array based representation of the image will be used later in order to prepare the
@@ -217,4 +157,5 @@ for image_path in TEST_IMAGE_PATHS:
       line_thickness=8)
   plt.figure(figsize=IMAGE_SIZE)
   plt.imshow(image_np)
+  plt.show()
 
